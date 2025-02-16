b5214213473658efee42eb6637f3b39ba01d2a49
Vasylyna Lykhogodenko <devvasylyna@gmail.com>
2025-02-16 13:57:03 +0200

    feat: add route get joke

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 require('dotenv').config();
 
 const Joke = require('./models/Joke');
@@ -10,8 +11,32 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const fetchRandomJoke = async () => {
+    try {
+      const response = await axios.get('https://teehee.dev/api/joke');
+      return {
+        question: response.data.setup,
+        answer: response.data.punchline,
+        votes: [],
+        availableVotes: ["😂", "👍", "❤️"],
+      };
+    } catch (err) {
+      console.error('Failed to fetch joke from external API:', err);
+      return null;
+    }
+};
+
 app.get('/api/joke', async (req, res) => {
-    
+    try {
+        const newJoke = await fetchRandomJoke();
+
+        if (newJoke) {
+          const savedJoke = await Joke.create(newJoke);
+          res.json(savedJoke);
+        }
+    } catch (err) {
+      res.status(500).json({ error: 'Failed to fetch a joke' });
+    }
 });
 
 app.listen(PORT, () => {
